480337dc7964dcd303dd9084eee9ff1c33b51aba
Armand <meskin.a@northeastern.edu>
2023-11-07 14:18:42 -0500

    Completed data collection

--- CollectData.py
+++ CollectData.py
@@ -7,36 +7,37 @@ import os
 # Start at 2018-11 (yr-month)
 year = 2018
 month = 11
+runningDict = dict()
+    
 
-while year <= 2024:
+while True:
     while month != 13:
         formatMonth = month
         if formatMonth < 10:
             formatMonth = "0" + str(formatMonth)
         url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NVDA&interval=1min&month={year}-{formatMonth}&outputsize=full&adjusted=false&datatype=json&extended_hours=false&apikey=3BAAT9BW5TUZBZY8'
         r = requests.get(url)
-        with open('data.json', 'w') as file:
-            if os.path.getsize("data.json") == 0:
-                json.dump(r.json()["Time Series (1min)"], file)
-            else:
-                file_data = json.load(file)
-
-                file_data.append(r.json()["Time Series (1min)"])
-        
-                file.seek(0)
-        
-                json.dump(file_data, file)
+        print("Request revieved...")
+        for key in r.json()["Time Series (1min)"]:
+            runningDict[key] = r.json()["Time Series (1min)"][key]
+        print(f"Completed entries for req for: {year} - {month}")
         month += 1
-        print("waiting 10 secs...")
-        time.sleep(5)
+        if year == 2023 and month == 12:
+            print("Five years accumulated...")
+            break
+        print("waiting 4 secs...")
+        time.sleep(4)
         print("avoided rate limit")
+    if year == 2023 and month == 12:
+        break
     month = 0
-    year += 1    
-    
+    year += 1
 
+with open('data.json', 'w') as file:
+    json.dump(runningDict, file, indent=4)
 #url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NVDA&interval=1min&month=2019-11&outputsize=full&adjusted=false&datatype=json&extended_hours=false&apikey=3BAAT9BW5TUZBZY8'
 #r = requests.get(url)
 #data = r.json()
 
 #print(data)
-#print("done")
+print("done")

8609fbe789bdc062d048c044744a7d57576d23cc
Armand <meskin.a@northeastern.edu>
2023-11-09 00:19:48 -0500

    Finished missing data repair

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,10 +1,11 @@
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Load data from json to an array
 def load_dat_array(filename):
     f = open(filename)
     dat = json.load(f)
+    f.close()
     arr_dat = [] 
     for sub_dict in dat.values():
         arr_dat.append(float(sub_dict['4. close']))
@@ -13,11 +14,13 @@ def load_dat_array(filename):
 # Load data from json to a dictionary
 def load_dat_dict(filename): 
     f = open(filename)
-    return json.load(f)
+    dat = json.load(f)
+    f.close()
+    return dat
 
 
 
-# Our data needs to be in chains of four weeks and we need to interpolate fake holiday data
+# Our data needs to be in chains of four weeks and we need to interpolate fake holiday and damaged data
 def process_dat(data):
     date_format = '%Y-%m-%d %H:%M:%S'
     data_truncate = data.copy()
@@ -32,9 +35,55 @@ def process_dat(data):
 
 
     # Fix damaged data that is missing minutes
-    for key in data_truncate.keys():
-        print("Fix the data...")
+    data_array = []
+    for id in data_truncate.keys():
+        data_array.append((id, data_truncate[id]))
 
+    index = 1
+    cleaned = False
+
+    while not cleaned:
+        cleaned = True
+        print("Beginning new runthrough")
+        for key in data_truncate.keys():
+            if index == len(data_truncate.keys()):
+                # Finished one passthrough
+                index = 1
+                break
+            day = datetime.strptime(key, date_format)
+            dayNext = day + timedelta(minutes=1)
+            if dayNext.strftime(date_format) == data_array[index][0] or '15:59:00' in key:
+                # All is well or we hit the end of a week
+                index += 1
+            else:
+                # Damaged data found, interpolate missing point
+                print(f"Damaged point found: {dayNext.strftime(date_format)} when comparing {data_array[index][0]}")
+                cleaned = False
+                open2 = float(data_truncate[key]['1. open']) + float(data_array[index][1]['1. open'])
+                high = float(data_truncate[key]['2. high']) + float(data_array[index][1]['2. high'])
+                low = float(data_truncate[key]['3. low']) + float(data_array[index][1]['3. low'])
+                close = float(data_truncate[key]['4. close']) + float(data_array[index][1]['4. close'])
+                volume = float(data_truncate[key]['5. volume']) + float(data_array[index][1]['5. volume'])
+
+                temp = dict()
+                temp['1. open'] = str(round(open2/2, 4))
+                temp['2. high'] = str(round(high/2, 4))
+                temp['3. low'] = str(round(low/2, 4))
+                temp['4. close'] = str(round(close/2, 4))
+                temp['5. volume'] = str(round(volume/2, 4))
+
+                # Insert fabricated point
+                data_array.insert(index, (dayNext.strftime(date_format), temp.copy()))
+                index += 2
+
+        # Update our main dict
+        print("Updating main dict")
+        data_truncate = dict(data_array)
+
+    #out = open("test-file.json", "w") 
+    #json.dump(data_truncate, out, indent=4)
+
+    
     indicator = 0
     holidays = []
     for key in data_truncate.keys():
